6b3020207a13ae6897c54eda261a6c2c1bcaafc3
kawiwattk <136226698+kawiwattk@users.noreply.github.com>
2023-06-26 22:19:41 +0700

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer() {
+    return (
+        <div className='MyFooter'>
+            <span>Copyright 2023 @ kawiwat thakon</span>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Header() {
+    return (
+        <div className='MyHeader'>
+            <h1>React Demo Test</h1>
+        </div>
+    );
+}
+
+export default Header;
